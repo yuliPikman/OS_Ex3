af28f5f8bdc804a73b8d8ad510df25b0f204ba75
ellorw.nir <nirellorwaizner@gmail.com>
2025-05-21 14:13:45 +0300

    Add googletest as an external project with updated configurations
    - Added CMake files for downloading and building googletest from the main branch.
    - Created necessary build and progress files for googletest.
    - Implemented custom commands for managing dependencies and build steps.
    - Ensured directory structure is created for source and build outputs.
    - Updated git commands to handle cloning and updating of the googletest repository.

---
+++ cmake-build-debug/mattanTests/CMakeFiles/mattanTester.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mattanTester.dir/SampleClient.cpp.o"
+  "CMakeFiles/mattanTester.dir/SampleClient.cpp.o.d"
+  "mattanTester"
+  "mattanTester.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mattanTester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
